f4459c80c255f6e5e1d3157eb80ef99a1a6abb14
fadeaway1992 <498497303@qq.com>
2017-05-24 16:42:04 +0800

    initted the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# 克隆云村音乐网
+
+> 基于 [NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseCloudMusicApi) 的 API 仿制的网易云音乐播放网站。
---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <h1 class="title">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Music Cloud'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
