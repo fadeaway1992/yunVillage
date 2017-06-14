885bfb73f28ce35a5d9a2706283fb722250a0567
fadeaway1992 <498497303@qq.com>
2017-06-14 14:42:15 +0800

    eslint

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,23 @@
-import Vue from 'vue'
 import axios from 'axios'
 // 设置请求地址头部
-var ycaxios = axios.create({
-  baseURL: 'http://localhost:3000/',
+const ycaxios = axios.create({
+  baseURL: 'http://localhost:3000/'
 })
 
-var httpUrlHeader
-if (process.env.NODE_ENV === 'development'){
-  httpUrlHeader = 'http://localhost:3000/'
-} else {
-  console.log("It's not development environment!")
-}
-
 export default{
-  loginByPhone (options, cb, errorCb) {
-    Vue.http.get(`${httpUrlHeader}login/cellphone`,options).then((response)=>{
-      cb(response)
-    }, (error)=>{
-      errorCb(error)
-    })
-  },
   // 使用手机登录
-  loginByAxios(options, cb, errorCb){
-    ycaxios.get('login/cellphone',options).then((response) => {
-      console.log(response)
-      cb(response)
+  loginByPhone (options) {
+    return ycaxios.get('login/cellphone', options).then((res) => {
+      return res
     })
   },
   // 获取用户歌单
-  getPlayList(id){
-    return ycaxios.get('user/playlist?uid=' + id).then( res => {
+  getPlayList (id) {
+    return ycaxios.get('user/playlist?uid=' + id).then(res => {
       console.log(res.data.playlist, '-------playList')
       return res.data.playlist
     })
-    .catch( e => {
+    .catch(e => {
       return e
     })
   }
--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -1,20 +1,20 @@
 import * as types from '../mutation-types'
 import api from '@/api'
 const state = {
-  userInfo:'',
-  loginInfo:'',
-  showLoginPad:false,
-  playlist:[]
+  userInfo: '',
+  loginInfo: '',
+  showLoginPad: false,
+  playlist: []
 }
 
 const mutations = {
-  [types.GET_USER](state,res){
-    console.log(res.status)
-    if(res.status!=200){
+  [types.GET_USER] (state, res){
+    console.log(res)
+    if (res.status != 200){
       state.loginInfo = '网络出现问题'
-    } else if (res.data.code!==200){
+    } else if (res.data.code !== 200) {
       state.loginInfo = '账户信息有误'
-    } else{
+    } else {
       state.showLoginPad = false
       state.userInfo = res
     }
@@ -25,7 +25,13 @@ const mutations = {
 }
 
 const actions = {
-  getUserPlayList: ({commit}, id) => {
+  loginByPhone: ({ commit }, options) => {
+    api.loginByPhone(options).then((res) => {
+      console.log(res, '-----打印登录请求返回的结果')
+      commit(types.GET_USER, res)
+    })
+  },
+  getUserPlayList: ({ commit }, id) => {
     api.getPlayList(id)
       .then( playlist => {
       commit(types.GET_USER_PLAYLIST, playlist)
