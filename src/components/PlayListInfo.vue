1fbeda0f2a3da5a414b7d0e5727c943b471d05d5
fadeaway1992 <498497303@qq.com>
2017-06-22 12:22:07 +0800

    歌单详情页面ui

--- src/components/PlayListInfo.vue
+++ src/components/PlayListInfo.vue
@@ -7,7 +7,9 @@
     <div class="content">
       <div class="head">
         <i class="icn-title"></i>
-        <div class="title"><h2 class="text-brk">{{listDetail.name}}</h2></div>
+        <div class="title">
+          <h2 class="text-brk">{{listDetail.name}}</h2>
+        </div>
       </div>
       <div class="user">
         <a class="face" href="javascript:;">
@@ -17,7 +19,14 @@
         <sup class="icon-star"></sup>
         <span class="create-time">{{formatDate(listDetail.createTime)}}&nbsp;创建</span>
       </div>
-      <div class="operations"></div>
+      <div class="operations">
+        <a href="javascript:;" class="play-list" hidefocus="true"><i><em class="play"></em>播放</i></a>
+        <a href="javascript:;" class="add" hidefocus="true"></a>
+        <a href="javascript:;" class="subscribe"><i class="icon-opteration">({{computedSubscribeCount()}})</i></a>
+        <a href="javascript:;" class="share"><i>({{listDetail.shareCount}})</i></a>
+        <a href="javascript:;" class="download"><i>下载</i></a>
+        <a href="javascript:;" class="comment"><i>({{listDetail.commentCount}})</i></a>
+      </div>
       <div class="tags"></div>
       <div class="description"></div>
       <div class="show-all">
@@ -28,28 +37,38 @@
 </template>
 
 <script>
-import { stampsToDate } from '@/assets/js/formatTime.js'
-export default {
-
-  props: {
-    listDetail: {}
-  },
-
-  methods: {
-    formatDate (stamp) {
-      return stampsToDate(stamp)
+  import {
+    stampsToDate
+  } from '@/assets/js/formatTime.js'
+  export default {
+    data () {
+      return {
+        subscribedCount: this.listDetail.subscribedCount
+      }
+    },
+    props: {
+      listDetail: {}
+    },
+    methods: {
+      formatDate (stamp) {
+        return stampsToDate(stamp)
+      },
+      computedSubscribeCount () {
+        if (this.subscribedCount > 100000) {
+          return Math.floor(this.subscribedCount / 10000) + '万'
+        } else {
+          return this.subscribedCount
+        }
+      }
+    },
+    mounted () {
+      console.log(this.listDetail, 'listDetail')
     }
-  },
-
-  mounted () {
-    console.log(this.listDetail, 'listDetail')
   }
-
-}
 </script>
 
 <style lang="scss" scoped>
-  .list-info{
+  .list-info {
     display: flex;
     justify-content: space-between;
   }
@@ -57,11 +76,11 @@ export default {
     width: 200px;
     height: 200px;
     position: relative;
-    .cover-img{
+    .cover-img {
       width: 100%;
       height: 100%;
     }
-    .msk{
+    .msk {
       position: absolute;
       width: 208px;
       height: 208px;
@@ -88,7 +107,7 @@ export default {
         margin-left: 64px;
         position: relative;
         line-height: 24px;
-        h2{
+        h2 {
           line-height: 24px;
           font-size: 20px;
           font-weight: normal;
@@ -130,6 +149,106 @@ export default {
     }
     .operations {
       margin-bottom: 25px;
+      .play-list {
+        &:hover {
+          background-position: right -510px;
+          i {
+            background-position: 0 -469px;
+            em {
+              background-position: -28px -1622px;
+            }
+          }
+        }
+        &:active {
+          background-position: right -592px;
+          i {
+            background-position: 0 -551px;
+            em {
+              background-position: -56px -1622px;
+            }
+          }
+        }
+        float: left;
+        color: #fff;
+        background: url(../assets/img/button2.png) no-repeat;
+        background-position: right -428px;
+        padding: 0 5px 0 0;
+        height: 31px;
+        line-height: 31px;
+        overflow: hidden;
+        text-align: center;
+        i {
+          display: block;
+          padding: 0 7px 0 8px;
+          background: url(../assets/img/button2.png) no-repeat;
+          background-position: 0 -387px;
+          height: 31px;
+          line-height: 31px;
+          overflow: hidden;
+          em {
+            float: left;
+            width: 20px;
+            height: 18px;
+            margin: 6px 2px 2px 0;
+            background: url(../assets/img/button2.png) no-repeat;
+            background-position: 0 -1622px;
+          }
+        }
+      }
+      .add {
+        float: left;
+        width: 31px;
+        height: 31px;
+        margin-right: 5px;
+        margin-left: -3px;
+        background: url(../assets/img/button2.png) no-repeat;
+        background-position: 0 -1588px;
+        &:hover {
+          background-position: -40px -1588px;
+        }
+        &:active {
+          background-position: -80px -1588px;
+        }
+      }
+      .subscribe {
+        cursor: default;
+        float: left;
+        height: 31px;
+        line-height: 30px;
+        padding: 0 5px 0 0;
+        margin-right: 6px;
+        background: url(../assets/img/button2.png) no-repeat;
+        background-position: right -1020px;
+        .icon-opteration {
+          float: left;
+          height: 31px;
+          padding-right: 2px;
+          padding-left: 28px;
+          background: url(../assets/img/button2.png) no-repeat;
+          background-position: 0 -977px;
+        }
+      }
+      .share {
+        @extend .subscribe;
+        i {
+          @extend .icon-opteration;
+          background-position: 0 -1225px;
+        }
+      }
+      .download {
+        @extend .subscribe;
+        i {
+          @extend .icon-opteration;
+          background-position: 0 -2761px;
+        }
+      }
+      .comment {
+        @extend .subscribe;
+        i {
+          @extend .icon-opteration;
+          background-position: 0 -1465px;
+        }
+      }
     }
     .tags {
       margin: 25px 0 5px;
@@ -140,8 +259,6 @@ export default {
       line-height: 18px;
       color: #666;
     }
-    .show-all{
-
-    }
+    .show-all {}
   }
 </style>
